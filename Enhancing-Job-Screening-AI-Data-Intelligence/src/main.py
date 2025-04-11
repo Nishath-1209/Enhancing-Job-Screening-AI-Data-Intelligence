ce838af4e5b42a2b0dd5778880ef8cf6485ed9c7
Nishath-1209 <snishathtabassum09@gmail.com>
2025-04-11 18:36:58 +0530

    Update main.py
    Added FastAPI app object in main.py

--- Enhancing-Job-Screening-AI-Data-Intelligence/src/main.py
+++ Enhancing-Job-Screening-AI-Data-Intelligence/src/main.py
@@ -1,2 +1,9 @@
 # Main Orchestration File
 print("AI Job Screening System Running...")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "AI Job Screening System is Working Successfully!"}
